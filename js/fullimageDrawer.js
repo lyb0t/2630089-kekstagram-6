ec119cc3174e458e1fd8932950554048a64de938
lyb0t <karanovak886@mail.ru>
2025-12-04 15:31:02 +0500

    taskfix

--- js/fullimageDrawer.js
+++ js/fullimageDrawer.js
@@ -8,18 +8,18 @@
 //   message: string,
 //   name: string
 // }[]
-const bigPicture = document.querySelector(".big-picture");
-const imageBlock = bigPicture.querySelector(".big-picture__img img");
-const likesBlock = bigPicture.querySelector(".likes-count");
-const commentsCount = bigPicture.querySelector(".comments-count");
-const commentsList = bigPicture.querySelector(".social__comments");
-const description = bigPicture.querySelector(".social__caption");
+const bigPicture = document.querySelector('.big-picture');
+const imageBlock = bigPicture.querySelector('.big-picture__img img');
+const likesBlock = bigPicture.querySelector('.likes-count');
+const commentsCount = bigPicture.querySelector('.comments-count');
+const commentsList = bigPicture.querySelector('.social__comments');
+const description = bigPicture.querySelector('.social__caption');
 
 const commentCounter = bigPicture.querySelector(
-  ".social__comment-count .comments-showed"
+  '.social__comment-count .comments-showed'
 );
-const commentLoader = bigPicture.querySelector(".comments-loader");
-const cancelBtn = bigPicture.querySelector(".big-picture__cancel");
+const commentLoader = bigPicture.querySelector('.comments-loader');
+const cancelBtn = bigPicture.querySelector('.big-picture__cancel');
 
 function generateCommentsHtml(comments) {
   const result = [];
@@ -35,18 +35,18 @@ function generateCommentsHtml(comments) {
             </li>
         `);
   }
-  return result.join("\n");
+  return result.join('\n');
 }
 
 let loaderFunction = null;
 export function drawFullImage(image) {
-  imageBlock.setAttribute("src", image.url);
+  imageBlock.setAttribute('src', image.url);
   likesBlock.innerHTML = image.likes;
   commentsCount.innerHTML = image.comments.length;
   description.innerHTML = image.description;
-  bigPicture.classList.remove("hidden");
-  commentLoader.classList.remove("hidden");
-  document.body.classList.add("modal-open");
+  bigPicture.classList.remove('hidden');
+  commentLoader.classList.remove('hidden');
+  document.body.classList.add('modal-open');
   let loadedCommentsCount = 0;
   const load5Comments = () => {
     const newComments = image.comments.slice(
@@ -57,24 +57,24 @@ export function drawFullImage(image) {
     loadedCommentsCount += 5;
     if (loadedCommentsCount >= image.comments.length) {
       loadedCommentsCount = image.comments.length;
-      commentLoader.classList.add("hidden");
+      commentLoader.classList.add('hidden');
     }
     commentCounter.innerHTML = loadedCommentsCount;
   };
   loaderFunction = load5Comments;
-  commentLoader.addEventListener("click", load5Comments);
+  commentLoader.addEventListener('click', load5Comments);
   load5Comments();
 }
 
 export function hideFullImage() {
-  bigPicture.classList.add("hidden");
-  document.body.classList.remove("modal-open");
-  commentLoader.removeEventListener("click", loaderFunction);
+  bigPicture.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  commentLoader.removeEventListener('click', loaderFunction);
 }
 
-cancelBtn.addEventListener("click", hideFullImage);
-document.body.addEventListener("keyup", (e) => {
-  if (e.key === "Escape") {
+cancelBtn.addEventListener('click', hideFullImage);
+document.body.addEventListener('keyup', (e) => {
+  if (e.key === 'Escape') {
     hideFullImage();
   }
 });
